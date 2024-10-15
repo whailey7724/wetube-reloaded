8c8dd67232e0e498f8679181367a0e1dc988f407
Whaliey <155971128+whailey7724@users.noreply.github.com>
2024-10-15 20:20:26 +0200

    #3.1 GET Requests ~ #3.3 Responses
    브라우저에서 get request가 들어왔을 때 request 들어온 것 콘솔창에서 확인하기, 들어온 request에 대해 반응하기
    ( res.end() , res.send() )

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+    return res.send("I'm here!! I'm still alive!!!");
+};
+
+const handleLogin = (req, res) => {
+    return res.send("Login here");
+}
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
